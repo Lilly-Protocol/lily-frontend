0f3a3eba973d6328330de61f757282c24fd34b65
root <root@srv1888275.hstgr.cloud>
2026-08-30 13:58:19 +0000

    chore: add import ordering via ESLint (#108)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,41 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  {
-    rules: {
-      "@typescript-eslint/consistent-type-imports": [
+ import importPlugin from "eslint-plugin-import";
+
+ const eslintConfig = defineConfig([
+   ...nextVitals,
+   ...nextTs,
+   {
+     plugins: {
+       import: importPlugin,
+     },
+     rules: {
+       "import/order": [
+         "error",
+         {
+           groups: [
+             "builtin",
+             "external",
+             "internal",
+             "parent",
+             "sibling",
+             "index",
+             "object",
+             "type",
+           ],
+           "newlines-between": "always",
+           alphabetize: {
+             order: "asc",
+             caseInsensitive: true,
+           },
+         },
+       ],
+     },
+   },
+   {
+     rules: {
+       "@typescript-eslint/consistent-type-imports": [
         "error",
         {
           prefer: "type-imports",
