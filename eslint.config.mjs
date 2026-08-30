4e17a6cf21b073496647676a2cb6097fa9751eb5
root <root@srv1888275.hstgr.cloud>
2026-08-30 03:13:34 +0000

    feat: add ESLint rule forbidding target=_blank without rel=noopener
    Closes #128

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,30 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import react from "eslint-plugin-react";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   {
+    plugins: {
+      react,
+    },
     rules: {
       "@typescript-eslint/consistent-type-imports": [
         "error",
         {
           prefer: "type-imports",
           fixStyle: "inline-type-imports",
+        },
+      ],
+      "react/jsx-no-target-blank": [
+        "error",
+        {
+          enforceDynamicLinks: "always",
+          warnOnSpreadAttributes: true,
+          allowReferrer: false,
+          forms: false,
         },
       ],
     },
