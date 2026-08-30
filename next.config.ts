a1f93327ae2a735dc189646992f8b947819f71dd
root <root@srv1888275.hstgr.cloud>
2026-08-30 14:02:42 +0000

    feat: configure images.remotePatterns for future OG and CDN assets (#126)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  typedRoutes: true,
-};
+ const nextConfig: NextConfig = {
+   typedRoutes: true,
+   images: {
+     remotePatterns: [
+       {
+         protocol: "https",
+         hostname: "**.githubusercontent.com",
+       },
+       {
+         protocol: "https",
+         hostname: "**.gravatar.com",
+       },
+       {
+         protocol: "https",
+         hostname: "**.twimg.com",
+       },
+       {
+         protocol: "https",
+         hostname: "**.cloudinary.com",
+       },
+       {
+         protocol: "https",
+         hostname: "**.unsplash.com",
+       },
+     ],
+   },
+ };
 
 export default nextConfig;
